a1ecba05c51619acac342cbb18d27cb5b558c585
Koome <judekoome360@gmail.com>
2023-06-27 09:39:54 +0300

    Done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
-end
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+    validate :clickbait_title
+  
+    private
+  
+    def clickbait_title
+      clickbait_phrases = ['Won\'t Believe', 'Secret', 'Top', 'Guess']
+      unless clickbait_phrases.any? { |phrase| title&.include?(phrase) }
+        errors.add(:title, 'must be clickbait-y')
+      end
+    end
+  end
+  
